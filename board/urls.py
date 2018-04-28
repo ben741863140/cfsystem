612dc4d46967209a2df63cf6035cda850baca1fd
ben741863140 <ben741863140@163.com>
2018-04-28 16:11:22 +0800

    更改了多榜系统的显示，仍有bug

--- board/urls.py
+++ board/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-from .views import board_upgrade
 from .views import board_rating
 from .views import board_rating_change
 
@@ -7,6 +6,6 @@ app_name = 'board'
 
 urlpatterns = [
     url(r'^$', board_rating),
-    url(r'^([0-9]+)$', board_upgrade),
+    url(r'^([0-9]+)$', board_rating),
     url(r'^profile/([0-9a-zA-Z_]+)', board_rating_change),
 ]
--- board/views.py
+++ board/views.py
@@ -1,52 +1,32 @@
 from django.shortcuts import render
-from board.models import CFUser, RatingChange
+from board.models import CFUser, RatingChange, Board, BoardItem
 from board.auto_update import auto_update
 
 
-def board_rating(request):
-    if request.user.is_authenticated:
-        class User:
-            rank = rating = handle = 0
-            realname = ''
-
-        users = []
-        for user in CFUser.objects.all():
-            info = User()
-            info.rating = user.rating
-            info.handle = user.handle
-            info.realname = user.realname
-            users.append(info)
-        users.sort(key=lambda x: x.rating, reverse=True)
-        for i in range(len(users)): users[i].rank = i + 1
-        return render(request, 'board/board_rating.html', {'users': users})
-    else:
-        return render(request, 'index.html')
-
-
-def board_upgrade(request, days_ago):
+def board_rating(request, id = -1):
     if request.user.is_authenticated:
         class User:
             rank = rating = handle = oldRating = newRating = change = 0
             realname = ''
-
         users = []
-        word = {'14': '两周', '30': '一月', '90': '三月', '180': '半年'}
-        time = '近' + word[str(days_ago)]
-        for rc in RatingChange.objects.filter(days_ago=days_ago):
+        if id == -1:
+            id = Board.objects.order_by('id').first().id
+        time = Board.objects.filter(id=id).get().name
+        for rc in BoardItem.objects.filter(board=Board.objects.filter(id=id).get()):
             user = User()
-            user.change = rc.newRating - rc.oldRating
+            user.change = rc.max_rating - rc.oldRating
             if user.change <= 0:
                 continue
             user.handle = rc.cf_user.handle
             user.oldRating = rc.oldRating
-            user.newRating = rc.newRating
+            user.newRating = rc.max_rating
             user.realname = rc.cf_user.realname
             users.append(user)
         users.sort(key=lambda x: x.change, reverse=True)
         for i in range(len(users)):
             users[i].rank = i + 1
             users[i].change = '+' + str(users[i].change)
-        return render(request, 'board/board_upgrade.html', {'users': users, 'time': time})
+        return render(request, 'board/board_upgrade.html', {'users': users, 'time': time, 'boards': Board.objects.all()})
     else:
         return render(request, 'index.html')
 
