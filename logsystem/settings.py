b1ab4aae56240e921b662a5bd563958ff2314084
root <root@localhost.localdomain>
2020-04-03 23:41:39 +0800

    更新了前端页面

--- logsystem/settings.py
+++ logsystem/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'd1q!t#2*rv=*n_s%5))&7&bho7t*8kr+_shx_ruhv@7%8biws0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['119.29.227.117', 'www.scaucf.top', '127.0.0.1']
+ALLOWED_HOSTS = ['119.29.227.117', 'www.scaucf.top', '127.0.0.1', 'scaucf.top']
 
 # Application definition
 
